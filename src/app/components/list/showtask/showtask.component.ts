4fba72e2ad61a94bf4249684576e5dd6614adb8f
MMAndresC <mandrescasabon@hotmail.com>
2022-06-21 14:21:28 +0200

    FEAT child showing list working

--- src/app/components/list/showtask/showtask.component.ts
+++ src/app/components/list/showtask/showtask.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-showtask',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./showtask.component.scss']
 })
 export class ShowtaskComponent implements OnInit {
-
+  @Input() public todoList!: string[];
   constructor() { }
 
   ngOnInit(): void {
